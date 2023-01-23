800b5ee5df0adfb140f277dd126c00e4b7924fe3
IgorZheltov1 <Saha21102014@gmail.com>
2023-01-23 20:15:03 +0300

    Update task1.py

--- Tasks/Zheltov/Task1/task1.py
+++ Tasks/Zheltov/Task1/task1.py
@@ -1,7 +1,7 @@
 import math
-a = int(input("Enter the amount in rubles:\n"))
-b = int(input("Term in years:\n"))
-c = int(input("Annual percentage:\n"))
+a = float(input("Enter the amount in rubles:\n"))
+b = float(input("Term in years:\n"))
+c = float(input("Annual percentage:\n"))
 
 g = (a*((1+(c/100))**b))
 
