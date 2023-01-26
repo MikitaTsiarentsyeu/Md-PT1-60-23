54388cbda532c63364a51096379b75fb51f6c7d0
che-kravchenko <123591706+che-kravchenko@users.noreply.github.com>
2023-01-27 01:47:16 +0300

    implement task1 version 2

--- Tasks/Kravchenko/task1/deposit.py
+++ Tasks/Kravchenko/task1/deposit.py
@@ -1,8 +1,7 @@
-import math
-ini_deposit = int(input())
-n_days = int(input())
-int_rate = float(input())
+ini_deposit = int(input('Введите сумму в рублях: \n'))
+n_days = int(input('Введите количество дней вклада: \n'))
+int_rate = float(input('Введите процентную ставку: \n'))
 int_income = ini_deposit*(int_rate/100)*(n_days/365)
 total_income = ini_deposit + int_income
-print('Баланс вашего счета составляет', math.floor(total_income), 'руб.', math.floor((total_income - math.floor(total_income))*100), 'коп.')
+print('Баланс вашего счета составляет', int(total_income), 'руб.', int((total_income - int(total_income))*100), 'коп.')
 
