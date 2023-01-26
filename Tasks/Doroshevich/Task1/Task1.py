2d8961d5ecf3c86e07913fc328c9eee9875b7167
Leonid <aau1991@gmail.com>
2023-01-26 15:22:39 +0400

    fix task1

--- Tasks/Doroshevich/Task1/Task1.py
+++ Tasks/Doroshevich/Task1/Task1.py
@@ -1,16 +1,22 @@
 import math
-start_amount = int(input("Введите сумму вклада в рублях:\n"))
-time = int(input("Введите срок вклада(количество лет):\n"))
-percent = int(input("Введите процентную ставку:\n"))
+start_amount = float(input("Enter the deposit amount:\n"))
+time = float(input("Enter the term of the deposit (number of years):\n"))
+percent = float(input("Enter interest rate:\n"))
 
 final_amount = start_amount + start_amount*time*percent/100
+final_amount = str(round(final_amount, 2)).split(".")
+rubles = final_amount[0]
+pennies = final_amount[1]
 
-if time % 10 == 1 and time % 100 != 11:
-    print("Сумма Вашего вклада через", time,
-          "год составит: " + str(final_amount) + "BYN")
-elif 1 < time % 10 < 5 and time % 100 not in range(12, 15):
-    print("Сумма Вашего вклада через", time,
-          "года составит: " + str(final_amount) + "BYN")
+if time == 1 and int(pennies) == 1:
+    print("The amount of your contribution after", time,
+          "year will be:", rubles, "rubles", pennies, "penny")
+elif time != 1 and int(pennies) == 1:
+    print("The amount of your contribution after", time,
+          "years will be: ", rubles, "rubles", pennies, "penny")
+elif time == 1 and int(pennies) != 1:
+    print("The amount of your contribution after", time,
+          "year will be: ", rubles, "rubles", pennies, "pennies")
 else:
-    print("Сумма Вашего вклада через", time,
-          "лет cоставит: " + str(final_amount) + "BYN")
+    print("The amount of your contribution after", time,
+          "years will be: ", rubles, "rubles", pennies, "pennies")
--- Tasks/Doroshevich/Task1/Task2.py
+++ Tasks/Doroshevich/Task1/Task2.py
@@ -1,4 +1,12 @@
 import math
 a, b, c, d = float(input("Введите коэффициент a:\n")), float(input("Введите коэффициент b:\n")), float(
     input("Введите коэффициент c:\n")), float(input("Введите коэффициент d:\n"))
-b1, c1, d1 = b/a, c/a, d/a
+print("Ваше уравнение имеет вид: " + str(a) + "x^3 + " +
+      str(b) + "x^2 + " + str(c) + "x + " + str(d) + " = 0")
+# Формула Кардано
+# Примем x = y - b/3a
+p = (3*a*c - b*b)/(3*a*a)
+q = (2*b*b*b - 9*a*b*c + 27*a*a*d)/(27*a*a*a)
+print("Канонический трехчленный вид: у^3 + " +
+      str(p) + "y + " + str(q) + " = 0")
+print(math.sqrt(-2))
