710806b93366376e76e07893ef90200aa0558121
RamaniukDarya <dadych@inbox.ru>
2023-01-27 14:13:17 +0300

    Rename

--- Tasks/Ramaniuk/Task1/Task1.py
+++ Tasks/Ramaniuk/Task1/Task1.py
@@ -11,7 +11,7 @@ else:
     print("As you make your bed, so you must lie in it. The amount on your account will be 0.")
 I = int(input())
 if I!=0:
-    S = P + (P * I / 100 * t)  #There are a lot of formulas for calculating, but I see no reason to use them if the task is that the input will be the amount, the term in years and the annual percentage.
+    S = P + (P * I / 100 * t)
     print("The amount on your account will be", S, "BYN.")
 else:
     print("As you make your bed, so you must lie in it. The amount on your account will be 0.")
