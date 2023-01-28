4048bda037f8cf39d73cb78ecb5078d326b2d294
Shalkevich Vladislav <shalkevichvladislav@gmail.com>
2023-01-28 12:40:38 +0300

    solutions 2
    I DID IT!

--- Tasks/Shalkevich/Task1/taskDop1.py
+++ Tasks/Shalkevich/Task1/taskDop1.py
@@ -1,3 +1,4 @@
+from  collections import Counter
 import pandas as pd
 # Что за не уважения к квадратам?) За кубическое уровнения 100 баллов, а за квадратное 0((
 print("Привет! ЧЕЛОВЕК. Я КОМПЬЮТЕР И Я УМНЕЕ ТЕБЯ! У меня памяти 16 мегабайт!!")
@@ -6,18 +7,34 @@ print()
 print("Я умею читать, писать, и... много чего умею. Вот ты сейчас напиши мне и я посчитаю всё!")
 while True:
     ansver = str(input("Введи, что угодно.Я могу всё! "))
-    Tex1 =len(ansver)
-    Tex = {'Пробел': 0, 'Букв': 0, 'Цифр': 0}
-    for i in ansver:
-        if i.isalpha():
-            Tex['Букв'] += 1
-        elif i.isdigit():
-            Tex['Цифр'] += 1
+    print("У меня есть 2 решения, какой ты хочещь увидеть? 1, 2?")
+    c = Counter(ansver)
+    try:
+        W = input()
+        W = int(W)
+        if W == 1:
+            Tex1 = len(ansver)
+            Tex = {'Пробел': 0, 'Букв': 0, 'Цифр': 0}
+            for i in ansver:
+                if i.isalpha():
+                    Tex['Букв'] += 1
+                elif i.isdigit():
+                    Tex['Цифр'] += 1
+                else:
+                    Tex['Пробел'] += 1
+            print("Так.. Сейчас посчитаю... так.. 1... Длина твоего текста ", Tex1,
+            " Символов! Из которых ", Tex, "из которых")
+            print()
+            print(c)
+        elif W ==2:
+            print(list(ansver))
+            print (pd.Series(list(ansver)).value_counts())
         else:
-            Tex['Пробел'] += 1
-    print("Так.. Сейчас посчитаю... так.. 1... Длина твоего текста ", Tex1, " Символов! Из которых ", Tex, "из которых")
-    print()
-    print (pd.Series(list(ansver)).value_counts())
+            print("Глуппый человек, я прошу выбрать 1 или 2 - это такие цифры, если не знаешь")
+            continue
+    except ValueError:
+        print("Глуппый человек, я прошу выбрать 1 или 2 - это такие цифры, если не знаешь")
+        continue
     print("Ты признаёшь мою победу, или повторить?) (1 - повторить), (2 - Признать, победу)")
     a = input()
     try:
