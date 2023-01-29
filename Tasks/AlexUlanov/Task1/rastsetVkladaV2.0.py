e9484e52aa936132cbe7bc25046b34af0fe5584f
Alex_Ulanov <ip-ulanov@mail.ru>
2023-01-29 19:32:27 +0300

    Update rastsetVkladaV2.0.py

--- Tasks/AlexUlanov/Task1/rastsetVkladaV2.0.py
+++ Tasks/AlexUlanov/Task1/rastsetVkladaV2.0.py
@@ -1,14 +1,17 @@
 def beSureIsDigit():
-     global x  
-     x = input()
-     while x.isdigit()==0:
-               x = input('ВЫ ВВЕЛИ СИМВОЛ А НЕ ЧИСЛО!!!ВВОДИТЕ ЗАНОВО!!!\n')
-               if x.isdigit()==1:
-                   break
+    global x  
+    x = input()
+    check = x.find(".")
+    if check == -1:
+        while (x.isdigit()==0 ):
+                    x = input('ВЫ ВВЕЛИ СИМВОЛ А НЕ ЧИСЛО!!!ВВОДИТЕ ЗАНОВО!!!\n')
+                    check = x.find(".")
+                    if check != -1:
+                        break
               
 print('Рассчитайте вашу прибыль от вклада.\nВведите сумму депозита в рублях\na=', end= ' ')
 beSureIsDigit()
-a = int(x)
+a = float(x)
 
    
 print('Введите количество месяцев\nb=', end= ' ')
@@ -18,7 +21,7 @@ b = int(x)
        
 print('Введите желаемый процент годовых\nс=', end= ' ')
 beSureIsDigit()
-c = int(x)
+c = float(x)
 c=c/12
 
 
@@ -29,4 +32,3 @@ for i in range (b):
     print(f'Сумма вашего вклада за {i+1} месяц {round(a,2)} рублей\n')
     
 
-       
