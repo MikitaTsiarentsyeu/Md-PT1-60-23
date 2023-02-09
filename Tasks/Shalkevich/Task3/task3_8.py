a0736a8bfb5d47da66688b151daf3ac62bdea64b
Shalkevich Vladislav <shalkevichvladislav@gmail.com>
2023-02-09 23:01:49 +0300

    oh

--- Tasks/Shalkevich/Task3/task3_8.py
+++ Tasks/Shalkevich/Task3/task3_8.py
@@ -1,17 +1,24 @@
 print("что-то большее, что-то меньшее, а где золотая середина??? Давай найдём золотую середину твоих чисел")
+nombers = []
 while True:
-    nomber1, nomber2 = input("число от которого считать числа"), input("число до которого считать числа")
+    nomber = input("чтобы выйти нажмите 0")
     try:
-        nomber1, nomber2 = int(nomber1), int(nomber2)
-        break
+        nomber = int(nomber)
+        if nomber !=0:
+            nombers.append(nomber)
+            continue
+        else:
+            break
     except ValueError:
         print("Это не число!")
 print("Отлично, теперь найдём золотую середину")
 count = 0
 summa = 0
-for i in range(nomber1, nomber2 + 1):
-    count =+ i
-    summa = summa + i
+for i in nombers:
+    count += 1
+    summa += i
+    summa = summa / count
+
 
 summa = summa / count
-print(" Итак... Посчитаем у вас чисел", count, "Золотая середина ваших чисел", round(summa))
+print(" Итак... ваши числа",nombers,"Посчитаем у вас чисел", count, "Золотая середина ваших чисел", round(summa))
