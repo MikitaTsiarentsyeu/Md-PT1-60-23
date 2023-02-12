1ca9549dbe7d64f1205f73383808feb4ee4d93ba
Mikhail Baboshko <118308869+baboha@users.noreply.github.com>
2023-02-12 15:47:40 +0300

    Delete task3_4.py

--- Tasks/Baboshko/Tasks 3/task3_4.py
+++
@@ -1,6 +0,0 @@
-new_numbers = input('введите любое количество чисел, пожалуйста: \n').split()
-max, max_2= int(new_numbers[0]), 0
-for i in range(1, len(new_numbers)):
-    if int(new_numbers[i]) >= max:
-        max, max_2 = int(new_numbers[i]), max
-print(max_2)
