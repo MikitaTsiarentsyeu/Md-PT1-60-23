8243471e9234ce931c04f123974500547b5b152e
Mikhail Baboshko <118308869+baboha@users.noreply.github.com>
2023-02-12 15:47:33 +0300

    Delete task3_6.py

--- Tasks/Baboshko/Tasks 3/task3_6.py
+++
@@ -1,8 +0,0 @@
-text_string = input('Пожалуйста, введите строку кирилицей: \n')
-new_string= ''
-for i in range(len(text_string)):
-    if text_string[i] in 'ёуеыаоэяию':
-        continue
-    else:
-        new_string = new_string + text_string[i]
-print(new_string)
