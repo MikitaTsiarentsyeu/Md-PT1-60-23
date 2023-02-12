ddd406707fdbe785dbbe95cbf51ece28b5b40ce1
Mikhail Baboshko <118308869+baboha@users.noreply.github.com>
2023-02-12 15:47:30 +0300

    Delete task3_7.py

--- Tasks/Baboshko/Tasks 3/task3_7.py
+++
@@ -1,8 +0,0 @@
-text_string = input('Пожалуйста, введите строку с заглавными и строчными буквами: \n')
-new_string = ''
-for i in range(len(text_string)):
-    if text_string[i].islower():
-        new_string = new_string + text_string[i].upper()
-    else:
-        new_string = new_string + text_string[i].lower()
-print(new_string, text_string.swapcase(), sep = '\n')#swapcase быстрее, конечно)
