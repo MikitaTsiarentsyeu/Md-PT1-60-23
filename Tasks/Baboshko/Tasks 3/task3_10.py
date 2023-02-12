17825db02efd6ebc3d456b7f6607533a22b1af28
Mikhail Baboshko <118308869+baboha@users.noreply.github.com>
2023-02-12 15:47:16 +0300

    Delete task3_10.py

--- Tasks/Baboshko/Tasks 3/task3_10.py
+++
@@ -1,9 +0,0 @@
-int_list = input('Пожалуйста, введите любое количество чисел: \n').split()
-for i in range(len(int_list)):
-    total, max = 0, 0
-    for j in range(1, int(int_list[i])+1):
-        if int(int_list[i]) % j == 0:
-            total += 1
-    if total == 2 and int(int_list[i]) > max:
-        max = int(int_list[i])
-print(max)
