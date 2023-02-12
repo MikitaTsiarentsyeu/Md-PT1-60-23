53c3670cccd58e57bafc8f5300f32ca09ffaa8bd
Mikhail Baboshko <118308869+baboha@users.noreply.github.com>
2023-02-12 15:47:27 +0300

    Delete task3_8.py

--- Tasks/Baboshko/Tasks 3/task3_8.py
+++
@@ -1,6 +0,0 @@
-int_list = input('Пожалуйста, введите любое количество чисел: \n').split()
-sum = 0
-for i in range(len(int_list)):
-    if int_list[i].isdigit():
-        sum += int(int_list[i])
-print('Среднее арифметическое списка чисел:', sum / len(int_list))
