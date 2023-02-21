0b7aeff5481bb926da464a7f50d5d07ed1512267
Mikhail Baboshko <118308869+baboha@users.noreply.github.com>
2023-02-21 23:18:23 +0300

    Create task5_1.py

---
+++ Tasks/Baboshko/Task 5/task5_1.py
@@ -0,0 +1,9 @@
+#first variant
+sum = lambda x, y: x+y
+print(sum(int(input()), int(input())))
+
+
+#second variant
+def sum(first, second):
+    return first + second
+print(sum(int(input()), int(input())))
