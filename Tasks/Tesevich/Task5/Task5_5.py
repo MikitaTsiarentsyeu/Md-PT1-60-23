9420a20026ff8a65b399268789357f6e258a68d8
Stukow <123178350+Stukow@users.noreply.github.com>
2023-02-22 17:34:08 +0300

    Update Task5_5.py

--- Tasks/Tesevich/Task5/Task5_5.py
+++ Tasks/Tesevich/Task5/Task5_5.py
@@ -1,43 +1,64 @@
 #def get_ranges(a):
-a = ("3 1 2 3 0 22 4 7 8 10 23 45 3 2 11 88")    
+data = [2, 3, 8, 9]  
 
-posl = []
-a1 = []
-itr = 1
+def delete_doubles(y):
+    rez = []
+    it = 1
+    for i in y:
+        if it > len(y)-1:
+            if y[-1] != y[-2]:
+                rez.append(y[-1])
+            break
+        if i != y [it]:
+            it = it +1
+            rez.append(i)
+        else:
+            it = it +1
+            continue
+    return rez
 
-a = a.split(" ")
-for i in a:
-    a1.append(int(i))
+def find_ranges(a1):
+    posl = [a1[0]]
+    itr = 1
+    for x in a1:
+        if itr == len(a1): # не уверен в этой проверке, как то не очень выглядит
+            if posl[-1] == "-": # не уверен в этой проверке, как то не очень выглядит
+                posl.append(a1[-1]) # не уверен в этой проверке, как то не очень выглядит
+                break
+            break
+        if x+1 == a1[itr]:
+            posl.append("-")    
+            itr =itr+1
+        else:
+            posl.append(x)
+            posl.append(a1[itr])
+            itr =itr+1
+    return posl
 
-a1 = set(a1)
-a1 = list(a1)
-a1 = sorted(a1)
-posl.append(a1[0])
+def get_ranges(data_1):
+    
+    rez = ''
 
+    data_1 = sorted(data_1)
+    data_1 = delete_doubles(data_1)
+    data_1 = find_ranges(data_1)
+    data_1 = delete_doubles(data_1)
+    i= 0
+    
+    for data_1[i] in data_1:
+        if i < (len(data_1)):
+            if data_1[i] is data_1[-1]:
+                rez = f'{rez}{data_1[i]}'
+                i=i+1
+                break
+            elif isinstance(data_1[i],int)and isinstance(data_1[i+1],int):
+                rez = f'{rez}{data_1[i]}, '
+                i=i+1
+            else:
+                rez = f'{rez}{data_1[i]}'
+                i=i+1       
+    return rez
 
+rezult = get_ranges(data)
+print (rezult)
 
-for x in a1:
-    if itr == len(a1): # не уверен в этой проверке, возможно стоило через ренжи
-        
-        break
-    if x+1 == a1[itr]:
-        posl.append("-")    
-        itr =itr+1
-    else:
-        posl.append(x)
-        posl.append(a1[itr])
-        itr =itr+1
-if posl[-2] == posl [-3]:
-    del posl[-2] #это наверное костыль, но на послдней проверке возникает дубль если она без промежутков
-
-posl = str(posl)
-# for it in posl:
-#     if it.isdigit():
-
-
-# posl = set(posl)
-# posl = list(posl)
-
-print(posl)
-
-#get_ranges([0, 1, 2, 3, 4, 7, 8, 10])  ->  "0-4, 7-8, 10"
