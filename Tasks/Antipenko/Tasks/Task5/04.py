b6b770b7094efd0c64d86062de55e584fdbe4b21
SrjaA <94987271+SrjaA@users.noreply.github.com>
2023-03-15 13:47:00 +0300

    Update 04.py

--- Tasks/Antipenko/Tasks/Task5/04.py
+++ Tasks/Antipenko/Tasks/Task5/04.py
@@ -1,11 +1,9 @@
 ''' Write a function that takes a string as an argument and returns two numbers, first for count of
 lower case symbols, second for count of the upper case symbols in the initial string. '''
 
-
 def register(s):
     a=0
     b=0
-
     for i in s:
         if i.islower():
             a+=1
