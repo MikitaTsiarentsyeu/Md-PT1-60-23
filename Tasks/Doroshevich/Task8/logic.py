e01a2b8a09dc30c9e18b9d9a4269c2aa795bb529
Leonid <aau1991@gmail.com>
2023-03-16 23:45:15 +0400

    done Task8

---
+++ Tasks/Doroshevich/Task8/logic.py
@@ -0,0 +1,34 @@
+import csv
+import sys
+
+
+def quit():
+    return sys.exit()
+
+
+def get_all_bracelets():
+    try:
+        with open("file.csv", 'r') as f:
+            reader = csv.DictReader(f)
+            l = [[(key + ': ' + value) for key, value in row.items()]
+                 for row in reader]
+            return '\n'.join([', '.join([y for y in x]) for x in l])
+    except FileNotFoundError:
+        return False
+
+
+def add_new_bracelet(material, color, size, sex):
+    with open("file.csv", 'a', newline='\n') as f:
+        writer = csv.writer(f)
+        writer.writerow([material, color, size, sex])
+
+
+def search(param, value):
+    try:
+        with open("file.csv", 'r') as f:
+            reader = csv.DictReader(f)
+            l = [[(key + ': ' + values) for key, values in row.items()
+                  if row[param] == value] for row in reader]
+            return '\n'.join([', '.join([y for y in x]) for x in l if x])
+    except FileNotFoundError:
+        return False
