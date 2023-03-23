490cbd1ac55ac12573a5679c6a179bef5684d8d5
Shalkevich Vladislav <shalkevichvladislav@gmail.com>
2023-03-23 23:29:01 +0300

    add

--- Tasks/Shalkevich/Task8/inter.py
+++ Tasks/Shalkevich/Task8/inter.py
@@ -2,80 +2,37 @@ import log
 
 def cycle():
     count = 0
-    film5 = log.Films(" ", " ", " ", " ")
-    film6 = log.Films(" ", " ", " ", " ")
-    film7 = log.Films(" ", " ", " ", " ")
-
     while True:
         print("\nВыберите пункт из меню:"
               "\n1. Добавить новую запись\n2. Вывести список всех альбомов\n3. Найти альбом или фильм\n4.Выйти.")
         answer = input()
         if answer == "1":
-            print("вы можете доабавить дополнительно не более чем три файла")
-            answer = input("хотите добавить файл?")
-            if answer == "Да":
-                    if count < 1:
-                        film5 = log.Nev_Film(5)
-                        count =+ 1
-                        continue
-                    elif count == 1:
-                        film6 = log.Nev_Film(6)
-                        count =+ 1
-                        continue
-                    else:
-                        film7 = log.Nev_Film(7)
-                        continue
+            print("вы можете доабавить дополнительнj 3 файла")
+            if count == 0:
+                log.film5 = log.Nev_Film()
+                count +=1
+            elif count == 1:
+                log.film6 = log.Nev_Film()
+                count += 1
+            elif count == 2:
+                log.film7 = log.Nev_Film()
             else:
-                continue
+                log.film10 = log.Nev_Film()
         elif answer == "2":
-            log.Films.alls(log.film1), log.Films.alls(log.film2),log.Films.alls(log.film3),log.Films.alls(log.film4),
-            log.Films.alls(film5), log.Films.alls(film6),log.Films.alls(film7)
+            log.ALL()
         elif answer == "3":
             print("\nВыберите элемент для поиска:\n"
                   "1.Поиск по названию\n2.Поиск по исполнителю/режиссёру\n3.Поиск по году\n4.Поиск по жанру")
-            answer2 = input()
-            if answer2 == "1":
-                answer = input("что ищим?")
-                log.filtr(answer)
-                if answer in film5.title:
-                    film5.alls()
-                elif answer in film6.title:
-                    film6.alls()
-                elif answer in film7.title:
-                    film7.alls()
-            elif answer2 == "2":
-                answer = input("что ищим?")
-                log.filtr_director(answer)
-                if answer in film5.director:
-                    film5.alls()
-                elif answer in film6.director:
-                    film6.alls()
-                elif answer in film7.director:
-                    film7.alls()
-            elif answer2 == "3":
-                answer = input("что ищим?")
-                log.filtr_year(answer)
-                if answer in film5.year:
-                    film5.alls()
-                elif answer in film6.year:
-                    film6.alls()
-                elif answer in film7.year:
-                    film7.alls()
-            elif answer2 == "4":
-                answer = input("что ищим?")
-                log.filtr_genre(answer)
-                if answer in film5.genre:
-                    film5.alls()
-                elif answer in film6.genre:
-                    film6.alls()
-                elif answer in film7.genre:
-                    film7.alls()
-                try:
-                    True
-                except AttributeError:
-                    continue
+            if answer2 == 1:
+                log.filtr(input("что искать?"))
+            elif answer2 == 2:
+                log.filtr_director(input("что искать?"))
+            elif answer2 == 3:
+                log.filtr_year(input("что искать?"))
+            elif answer2 == 4:
+                log.filtr_genre(input("что искать?"))
             else:
-                break
+                continue
         elif answer == "4":
             break
 
